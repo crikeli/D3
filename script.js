ac18bb4a1d4002f49191646d7a7cfc4471f8e120
Kelin Christi <christi.kelin20@gmail.com>
2016-03-26 16:11:25 -0700

    Learnt how to scale a bar chart based on the largest value

--- script.js
+++ script.js
@@ -73,3 +73,53 @@ d3.selectAll('.item')
         return d.width + 'px';
     }
 })
+
+d3.select("#containerthree")
+    .append("svg")
+    .attr("width", 600)
+    .attr("height", 400)
+    .style("background", "#93A1A1")
+// .append("rect")
+// .attr("x", 200)
+// .attr("y", 100)
+// .attr("height", 200)
+// .attr("width", 200)
+// .style("fill", "#CB4B19")
+// d3.select("svg")
+.append("circle")
+    .attr("cx", 300)
+    .attr("cy", 200)
+    .attr("r", 50)
+    .style("fill", "#840043")
+
+
+//Last 4-04 and 5-01
+var bardata = [50, 100, 200, 300, 400, 500, 60, 88, 90, 99, 102, 188, 600];
+
+var height = 400,
+    width = 600,
+    barWidth = 50,
+    barOffset = 5
+
+//Allows for the mapping of a series of values from the domain onto a range
+var yScale = d3.scale.linear()
+    .domain([0, d3.max(bardata)])
+    .range([0, height])
+
+d3.select("#bchart").append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .style("background", '#C9D7D6')
+    .selectAll('rect').data(bardata)
+    .enter().append('rect')
+    .style('fill', '#C61C6F')
+    .attr('width', barWidth)
+    .attr('height', function(d) {
+        return yScale(d);
+    })
+    .attr('x', function(d, i) {
+        return i * (barWidth + barOffset);
+    })
+    .attr('y', function(d) {
+        return height - yScale(d);
+    })
